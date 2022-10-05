0e1e168d35292bb50fb98ea787b25b6361f3a1ff
Dionisio <dionisio@malteso.com>
2022-10-05 17:18:45 -0400

    Minus operator

--- app/src/main/java/com/dionisio/calculator/MainActivity.kt
+++ app/src/main/java/com/dionisio/calculator/MainActivity.kt
@@ -37,5 +37,59 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun onOperator(view: View){
+        tvInput?.text?.let {
+            if(lastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+
+    }
+
+    fun onEqual(view: View) {
+        if(lastNumeric){
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+            if(tvValue.startsWith("-")){
+                prefix="-"
+                tvValue = tvValue.substring(1)
+            }
+
+            try {
+
+                if(tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if(prefix.isNotEmpty()){
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = (one.toDouble() - two.toDouble()).toString()
+                }
+
+            }
+            catch (e : ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+    private fun isOperatorAdded(value: String) : Boolean {
+        return if(value.startsWith("-")){
+            false
+        } else {
+            value.contains('/')
+                || value.contains("*")
+                || value.contains("+")
+                || value.contains("-")
+        }
+    }
+
   
 }
